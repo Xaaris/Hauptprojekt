Introduced tf debug, changed taking center square to just squashing the image ignoreing aspect ratio, fixed scaling back up,

main.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     yolo = YOLO()
     start = time.time()
     for frame_counter, frame in enumerate(get_frames("testFiles/IMG_2993.m4v", 0, 1)):
-        frame = take_center_square(frame)
         frame_copy = np.copy(frame)
         vehicle_boxes = yolo.detect_vehicle(frame)
-        for vehicle_box in vehicle_boxes:
+        for vehicle_counter, vehicle_box in enumerate(vehicle_boxes):
             top, left, bottom, right = vehicle_box
             image_copy = cv2.rectangle(frame_copy, (left, top), (right, bottom), (0, 0, 255), 2)
             car_image = get_image_patch(frame, vehicle_box)
+            save_debug_image(car_image, "frame_" + str(frame_counter) + "_car_" + str(vehicle_counter), "found_vehicles")
             license_plate_detection = LicensePlateDetection(car_image)
             plate_candidates = license_plate_detection.detect_license_plate_candidates()
             cv2.drawContours(frame_copy, plate_candidates, -1, (127, 0, 255), 2, offset=(left, top))

yolo.py
@@ -12,6 +12,7 @@ from keras.utils import multi_gpu_model
 from model import yolo_eval
 from timer import timing
 from utils import resize_image
+from tensorflow.python import debug as tf_debug
 
 
 @timing
@@ -40,6 +41,9 @@ class YOLO(object):
         self.class_names = self._get_class()
         self.anchors = self._get_anchors()
         self.sess = K.get_session()
+        # sess = tf_debug.LocalCLIDebugWrapperSession(self.sess)
+        # K.set_session(sess)
+        # self.sess = K.get_session()
         self.boxes, self.scores, self.classes = self.generate()
 
     def _get_class(self):
@@ -68,6 +72,7 @@ class YOLO(object):
                 num_classes + 5), 'Mismatch between model and given anchor and class sizes'
 
         print('{} model, anchors, and classes loaded.'.format(model_path))
+        print(self.yolo_model.summary())
 
         # Generate output tensor targets for filtered bounding boxes.
         self.input_image_shape = K.placeholder(shape=(2,))
@@ -79,6 +84,7 @@ class YOLO(object):
 
     @timing
     def detect_vehicle(self, image):
+        height, width, _ = image.shape
         resized_image = resize_image(image, self.model_image_size)
         image_data = np.array(resized_image, dtype='float32')
 
@@ -90,7 +96,7 @@ class YOLO(object):
             [self.boxes, self.scores, self.classes],
             feed_dict={
                 self.yolo_model.input: image_data,
-                self.input_image_shape: [image.shape[1], image.shape[0]],
+                self.input_image_shape: [height, width],
                 K.learning_phase(): 0
             })
 