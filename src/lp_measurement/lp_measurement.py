1603e68f232fa86761a5124c4185f60859a40a6b
Hannes <johannes.berger1@haw-hamburg.de>
2019-03-10 17:11:15 +0100

    WIP: detecting upper and lower bounds of lp

--- src/lp_measurement/lp_measurement.py
+++ src/lp_measurement/lp_measurement.py
@@ -23,11 +23,12 @@ def line_is_horizontal(line):
 
 
 def draw_lines(image, lines):
+    image_copy = np.copy(image)
     if lines is not None:
         for line in lines:
-            x1, y1, x2, y2 = line
-            cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 1)
-    return image
+            color = list(np.random.random(size=3) * 256)
+            cv2.line(image_copy, line[0], line[1], color, 1)
+    return image_copy
 
 
 def correct_white_balance(image):
@@ -36,7 +37,6 @@ def correct_white_balance(image):
 
 
 def detect_vertical_lines(gray_image):
-
     v_edges = cv2.Sobel(gray_image, -1, 1, 0)
     v_edges = cv2.Canny(v_edges, 50, 150, apertureSize=3)
     show(v_edges, "v_edges")
@@ -54,7 +54,6 @@ def detect_vertical_lines(gray_image):
 
 
 def detect_horizontal_lines(gray_image):
-
     h_edges = cv2.Sobel(gray_image, -1, 0, 1)
     h_edges = cv2.Canny(h_edges, 50, 150, apertureSize=3)
     show(h_edges, "h_edges")
@@ -71,6 +70,23 @@ def detect_horizontal_lines(gray_image):
         return horizontal_lines
 
 
+def get_lines_from_contour(contour):
+    lines = []
+    for i in range(len(contour)):
+        j = (i + 1) % len(contour)
+        lines.append(((contour[i][0][0], contour[i][0][1]), (contour[j][0][0], contour[j][0][1])))
+    return lines
+
+
+def get_2_longest_lines_from_contour(contour):
+    lines = []
+    for i in range(len(contour)):
+        j = (i + 1) % len(contour)
+        lines.append(((contour[i][0][0], contour[i][0][1]), (contour[j][0][0], contour[j][0][1])))
+    two_longest_lines = sorted(lines, key=get_line_length, reverse=True)[:2]
+    return two_longest_lines
+
+
 def find_lp_contour(image):
     # convert to HSV color scheme
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -78,63 +94,74 @@ def find_lp_contour(image):
     hue, saturation, value = cv2.split(hsv_image)
     # show(hue, "hue")
     # show(saturation, "saturation")
-    # show(value, "value")
+    show(value, "value")
     # threshold to find the contour
     _, thresholded = cv2.threshold(value, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    # show(thresholded, "thresholded")
+    show(thresholded, "thresholded")
     # morphological operations
     kernel = (7, 7)
     thresholded_close = cv2.morphologyEx(thresholded, cv2.MORPH_CLOSE, kernel)
-    # show(thresholded_close, "thresholded_close")
+    show(thresholded_close, "thresholded_close")
     thresholded_open = cv2.morphologyEx(thresholded_close, cv2.MORPH_OPEN, kernel)
-    # show(thresholded_open, "thresholded_open")
+    show(thresholded_open, "thresholded_open")
     # The cv2.findContours method is destructive (meaning it manipulates the image you pass in)
     # so if you plan on using that image again later, be sure to clone it.
     contours = cv2.findContours(thresholded_open.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
     # keep only 10 the largest ones
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+    biggest_contour = sorted(contours, key=cv2.contourArea, reverse=True)[0]
     # the contour that we seek for
     # loop over our 10 largest contours in the query image
-    # for contour in contours:
-    #     image_copy = np.copy(image)
-    #     cv2.drawContours(image_copy, [contour], -1, (0, 255, 0), 1)
-    #     show(image_copy, "all contours")
-    for contour in contours:
-        # approximate the contour
-        # These methods are used to approximate the polygonal curves of a contour.
-        # In order to approximate a contour, you need to supply your level of approximation precision.
-        # In this case, we use 2% of the perimeter of the contour. The precision is an important value to consider.
-        # If you intend on applying this code to your own projects, you’ll likely have to play around with the precision value.
-        perimeter = cv2.arcLength(contour, True)
-        approx_poly = cv2.approxPolyDP(contour, 0.05 * perimeter, True)
-        # if our approximated contour has four points, then
-        # we can assume that we have found our screen
-        # image_copy2 = np.copy(image)
-        # cv2.drawContours(image_copy2, [approx_poly], -1, (0, 255, 0), 1)
-        # show(image_copy2, "approx")
-        if len(approx_poly) == 4:
-            return approx_poly
+    image_copy = np.copy(image)
+    cv2.drawContours(image_copy, [biggest_contour], -1, (0, 255, 0), 1)
+    show(image_copy, "all contours")
+
+    perimeter = cv2.arcLength(biggest_contour, True)
+    approx_poly = cv2.approxPolyDP(biggest_contour, 0.01 * perimeter, True)
+    image_copy2 = np.copy(image)
+    cv2.drawContours(image_copy2, [approx_poly], -1, (0, 255, 0), 1)
+    show(image_copy2, "approx")
+    return approx_poly
 
 
+def perfect_line(image, line):
+    pt1, pt2 = line
+    dx, dy = pt1[0] - pt2[0], pt1[1] - pt2[1]
+    angle_rad = math.atan2(dy, dx)
+    points_on_line = getEquidistantPoints(pt1, pt2, 10)
+    for point in points_on_line:
+        first_point = get_point_at(point, 10, angle_rad + np.pi / 2)
+        second_point = get_point_at(point, -10, angle_rad + np.pi / 2)
+        cv2.line(image, first_point, second_point, (0, 0, 255), 1)
+    show(image)
 
 
+def getEquidistantPoints(pt1, pt2, num_of_points):
+    return zip(np.linspace(pt1[0], pt2[0], num_of_points + 1, dtype=int),
+               np.linspace(pt1[1], pt2[1], num_of_points + 1, dtype=int))
 
-test_image_path = "../lp_validation/data/train/positives/3227759838959127893.png"
+
+def get_point_at(origin, dist, theta):
+    return int(origin[0] + dist * math.cos(theta)), int(origin[1] + dist * math.sin(theta))
+
+
+def get_line_length(line):
+    pt1, pt2 = line
+    dx, dy = pt1[0] - pt2[0], pt1[1] - pt2[1]
+    length = math.hypot(dx, dy)
+    return length
+
+
+test_image_path = "3590476308847402370.png"
 image = load_image(test_image_path)
-show(image, "input")
 balanced_image = correct_white_balance(image)
-# show(balanced_image, "balanced")
+show(balanced_image, "balanced")
 
 lp_contour = find_lp_contour(balanced_image)
-cv2.drawContours(balanced_image, [lp_contour], -1, (0, 255, 0), 1)
-show(balanced_image, "final")
 
+lines = get_2_longest_lines_from_contour(lp_contour)
+image_with_lines = draw_lines(balanced_image, lines)
+show(image_with_lines, "image_with_lines")
 
-# gray_image = cv2.cvtColor(balanced_image, cv2.COLOR_BGR2GRAY)
-# show(gray_image, "gray")
-#
-# vertical_lines = detect_vertical_lines(gray_image)
-# horizontal_lines = detect_horizontal_lines(gray_image)
-#
-# image_with_lines = draw_lines(image, horizontal_lines + vertical_lines)
-# show(image_with_lines, "final")
+for line in lines:
+    perfect_line(image, line)
+
