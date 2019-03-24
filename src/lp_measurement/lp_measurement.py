8e97d7a5722e8d1e11304c5a228f7eb53cbd8a8b
johannesberger <xaaris@googlemail.com>
2019-03-24 19:09:57 +0100

    Getting height of LP now

--- src/lp_measurement/lp_measurement.py
+++ src/lp_measurement/lp_measurement.py
@@ -11,23 +11,12 @@ def load_image(path):
     return cv2.imread(path)
 
 
-def line_is_vertical(line):
-    x1, y1, x2, y2 = line
-    angle = math.atan2(y1 - y2, x1 - x2) * 180 / math.pi
-    return True if 80 < abs(angle) < 100 else False
-
-
-def line_is_horizontal(line):
-    x1, y1, x2, y2 = line
-    angle = math.atan2(y1 - y2, x1 - x2) * 180 / math.pi
-    return True if abs(angle) < 10 or abs(angle) > 170 else False
-
-
-def draw_lines(image, lines):
+def draw_lines(image, lines, color=None):
     image_copy = np.copy(image)
     if lines is not None:
         for line in lines:
-            color = list(np.random.random(size=3) * 256)
+            if color is None:
+                color = list(np.random.random(size=3) * 256)
             cv2.line(image_copy, line[0], line[1], color, 1)
     return image_copy
 
@@ -35,14 +24,6 @@ def draw_lines(image, lines):
 def correct_white_balance(image):
     wb = cv2.xphoto.createSimpleWB()
     return wb.balanceWhite(image)
-
-
-def get_lines_from_contour(contour):
-    lines = []
-    for i in range(len(contour)):
-        j = (i + 1) % len(contour)
-        lines.append(((contour[i][0][0], contour[i][0][1]), (contour[j][0][0], contour[j][0][1])))
-    return lines
 
 
 def get_2_longest_lines_from_contour(contour):
@@ -55,38 +36,22 @@ def get_2_longest_lines_from_contour(contour):
 
 
 def find_lp_contour(image):
-    # convert to HSV color scheme
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # split HSV to three chanels
     hue, saturation, value = cv2.split(hsv_image)
-    # show(hue, "hue")
-    # show(saturation, "saturation")
-    # show(value, "value")
-    # threshold to find the contour
     _, thresholded = cv2.threshold(value, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     show(thresholded, "thresholded")
-    # morphological operations
     kernel = (7, 7)
     thresholded_close = cv2.morphologyEx(thresholded, cv2.MORPH_CLOSE, kernel)
-    # show(thresholded_close, "thresholded_close")
     thresholded_open = cv2.morphologyEx(thresholded_close, cv2.MORPH_OPEN, kernel)
-    # show(thresholded_open, "thresholded_open")
-    # The cv2.findContours method is destructive (meaning it manipulates the image you pass in)
-    # so if you plan on using that image again later, be sure to clone it.
     contours = cv2.findContours(thresholded_open.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
-    # keep only 10 the largest ones
     biggest_contour = sorted(contours, key=cv2.contourArea, reverse=True)[0]
-    # the contour that we seek for
-    # loop over our 10 largest contours in the query image
     image_copy = np.copy(image)
     cv2.drawContours(image_copy, [biggest_contour], -1, (0, 255, 0), 1)
-    # show(image_copy, "all contours")
 
     perimeter = cv2.arcLength(biggest_contour, True)
     approx_poly = cv2.approxPolyDP(biggest_contour, 0.01 * perimeter, True)
     image_copy2 = np.copy(image)
     cv2.drawContours(image_copy2, [approx_poly], -1, (0, 255, 0), 1)
-    # show(image_copy2, "approx")
     return approx_poly
 
 
@@ -112,11 +77,10 @@ def perfect_line(image, line):
         if line_point_in_array is not None:
             actual_line_point = get_point_at(first_point, line_point_in_array, angle_rad - np.pi / 2)
             actual_line_points.append(actual_line_point)
-            cv2.line(image, actual_line_point, get_point_at(actual_line_point, ten_percent_of_image_height, angle_rad), (0, 255, 0), 1)
+            # cv2.line(image, actual_line_point, get_point_at(actual_line_point, ten_percent_of_image_height, angle_rad), (0, 255, 0), 1)
 
     line_start, line_end = best_fit_line_from_points(actual_line_points)
-    cv2.line(image, line_start, line_end, (255, 0, 0), 1)
-    show(image)
+    return line_start, line_end
 
 
 def best_fit_line_from_points(points):
@@ -157,7 +121,37 @@ def get_line_length(line):
     return length
 
 
-test_image_path = "3227759838959127893.png"
+def get_nearest_point_on_line(line, point):
+    pt1, pt2 = line
+    x1, y1 = pt1
+    x2, y2 = pt2
+    x3, y3 = point
+
+    k = ((y2 - y1) * (x3 - x1) - (x2 - x1) * (y3 - y1)) / ((y2 - y1) ** 2 + (x2 - x1) ** 2)
+    x4 = x3 - k * (y2 - y1)
+    y4 = y3 + k * (x2 - x1)
+    if min(x1, x2) <= x4 <= max(x1, x2):
+        return x4, y4
+
+
+def get_average_distance_of_lines(line1, line2):
+    inner_edge_lines = []
+    for pt in line1:
+        lot_fuss = get_nearest_point_on_line(line2, pt)
+        if lot_fuss is not None:
+            inner_edge_lines.append([lot_fuss, pt])
+    for pt in line2:
+        lot_fuss = get_nearest_point_on_line(line1, pt)
+        if lot_fuss is not None:
+            inner_edge_lines.append([lot_fuss, pt])
+    assert len(inner_edge_lines) == 2
+    average_distance_between_lines = 0
+    for line in inner_edge_lines:
+        average_distance_between_lines += get_line_length(line)
+    return average_distance_between_lines / 2
+
+
+test_image_path = "2505119030049077848.png"
 image = load_image(test_image_path)
 balanced_image = correct_white_balance(image)
 show(balanced_image, "balanced")
@@ -165,8 +159,10 @@ show(balanced_image, "balanced")
 lp_contour = find_lp_contour(balanced_image)
 
 lines = get_2_longest_lines_from_contour(lp_contour)
-image_with_lines = draw_lines(balanced_image, lines)
+image_with_lines = draw_lines(balanced_image, lines, (0, 0, 255))
 show(image_with_lines, "image_with_lines")
 
-for line in lines:
-    perfect_line(image, line)
+final_lines = [perfect_line(image, line) for line in lines]
+image_with_lines = draw_lines(image_with_lines, final_lines, (0, 255, 0))
+show(image_with_lines, "image_with_lines")
+print(get_average_distance_of_lines(final_lines[0], final_lines[1]))
