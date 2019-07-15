2806964eaafbbb49ab13313e9ffa2fb4a82ce21b
johannesberger <xaaris@googlemail.com>
2019-07-15 15:39:09 +0200

    Using Python 3.7 dataclasses

--- src/Video.py
+++ src/Video.py
@@ -1,40 +1,40 @@
+from dataclasses import dataclass, field
+
 from numpy.core.multiarray import ndarray
 
 
+@dataclass
 class Plate:
-    box: [int, int, int, int]
     valid = False
-    confidence: float
-    height: float
+    confidence: float = 0
+    height: float = 0
+    box: [int, int, int, int] = field(default_factory=list)
 
 
+@dataclass
 class Vehicle:
-    box: [int, int, int, int]
-    plates: [Plate]
+    box: [int, int, int, int] = field(default_factory=list)
+    plates: [Plate] = field(default_factory=list)
 
 
+@dataclass
 class Frame:
     """Class for keeping track of data found within a frame"""
     frame_number: int
     image: ndarray
-    vehicles: [Vehicle] = []
-
-    def __init__(self, frame_number, image):
-        self.frame_number = frame_number
-        self.image = image
+    vehicles: [Vehicle] = field(default_factory=list)
 
     def __str__(self):
-        retString = "Frame: " + str(self.frame_number)
+        ret_string = "Frame: " + str(self.frame_number)
         for index, vehicle in enumerate(self.vehicles):
-            retString += "  Vehicle: " + str(index)
+            ret_string += "  Vehicle: " + str(index)
             for plate in vehicle.plates:
-                if (plate.valid and hasattr(plate, "height")):
-                    retString += " plate height: " + "{:.4f}".format(plate.height)
-        return retString
+                if plate.valid and hasattr(plate, "height"):
+                    ret_string += " plate height: " + "{:.4f}".format(plate.height)
+        return ret_string
 
 
+@dataclass
 class Video:
-    frames: [Frame] = []
-
-    def __init__(self, path_to_file):
-        self.path_to_file = path_to_file
+    path_to_file: str
+    frames: [Frame] = field(default_factory=list)
